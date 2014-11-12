ce9adbc96ed8f9b20eae1ea7d478b047e31e92e2
Gennady Novoselov <gn@isfb.ru>
2014-11-12 18:48:39 +0300

    BROWNGOODS-310 - Изображения новостей - ошибка отображения

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -2,6 +2,6 @@ class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment, 
      :styles => IMAGE_STYLES,
      :default_style => :large,
-     :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
-     :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"  
+     :url => "/blog_entry_images/:id/:style/:basename.:extension",
+     :path => ":rails_root/public/blog_entry_images/:id/:style/:basename.:extension"
 end
